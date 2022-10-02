170ded5e60284651106a8da11d479241197de6a2
nasir-gaayte <slowboyz12@gmail.com>
2022-10-02 10:47:42 +0300

    edit

--- Hotel/Hotel/settings.py
+++ Hotel/Hotel/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
 import os
-# import django_heroku
+import heroku as django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,4 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
